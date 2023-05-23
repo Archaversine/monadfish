2066e1158976306b82eb20fcefd97e71c1dbb77c
Archaversine <adam.brohl.w@gmail.com>
2023-05-23 11:02:52 -0500

    Commented mask pawn code

--- src/Attacks.hs
+++ src/Attacks.hs
@@ -20,10 +20,18 @@ generatePawnAttacks = undefined
 maskPawnAttacks :: BoardSquare -> BoardColor -> Bitboard 
 maskPawnAttacks square color = do 
     let piece = setSquareBit square 0
+
+        -- List of functions that shifts the piece bit to 
+        -- appropriate attack positions
         attacks = map (flip shifter) [7, 9]
+
+        -- Mask out the attacks that go off the board
         edges = notAFile .&. notHFile
+
+        -- Shift direction is dependent on piece color
         shifter = case color of 
             White -> shiftR 
             Black -> shiftL
 
+    -- Apply the list of functions to the bitboard
     foldl' (\acc f -> acc .|. f piece) 0 attacks .&. edges
